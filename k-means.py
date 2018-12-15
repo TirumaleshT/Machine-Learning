99eb307c9f28018c90ed6098d4966af1bfc344d8
Tirumalesh <45584407+TirumaleshT@users.noreply.github.com>
2018-12-15 17:27:29 +0530

    K -means

---
+++ k-means.py
@@ -0,0 +1,48 @@
+def kmeans(k, epsilon=0, distance='euclidian'):
+    history_centroids = []
+    if distance == 'euclidian':
+        dist_method = euclidian
+    dataset = load_dataset('pulse_classify2.csv')
+    # dataset = dataset[:, 0:dataset.shape[1] - 1]
+    num_instances, num_features = dataset.shape
+    prototypes = dataset[np.random.randint(0, num_instances - 1, size=k)]
+    history_centroids.append(prototypes)
+    prototypes_old = np.zeros(prototypes.shape)
+    belongs_to = np.zeros((num_instances, 1))
+    norm = dist_method(prototypes, prototypes_old)
+    iteration = 0
+    while norm > epsilon:
+        iteration += 1
+        norm = dist_method(prototypes, prototypes_old)
+        prototypes_old = prototypes
+        for index_instance, instance in enumerate(dataset):
+            dist_vec = np.zeros((k, 1))
+            for index_prototype, prototype in enumerate(prototypes):
+                dist_vec[index_prototype] = dist_method(prototype,
+                                                        instance)
+
+            belongs_to[index_instance, 0] = np.argmin(dist_vec)
+
+        tmp_prototypes = np.zeros((k, num_features))
+
+        for index in range(len(prototypes)):
+            instances_close = [i for i in range(len(belongs_to)) if belongs_to[i] == index]
+            prototype = np.mean(dataset[instances_close], axis=0)
+            # prototype = dataset[np.random.randint(0, num_instances, size=1)[0]]
+            tmp_prototypes[index, :] = prototype
+
+        prototypes = tmp_prototypes
+
+        history_centroids.append(tmp_prototypes)
+
+    # plot(dataset, history_centroids, belongs_to)
+
+    return prototypes, history_centroids, belongs_to
+
+
+def execute():
+    dataset = load_dataset('pulse_classify2.csv')
+    #print(dataset)
+    centroids, history_centroids, belongs_to = kmeans(3)
+
+execute()
